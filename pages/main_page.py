c852107a5234d83deab49f740f90acd97c284f4e
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-01-12 09:51:13 +0300

    Page Object для главной страницы сайта

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,9 +4,9 @@ from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
+    def go_to_login_page(self): # метод, который будет проверять переход по ссылке на страницу логина
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
 
-    def should_be_login_link(self):
+    def should_be_login_link(self): # метод, который будет проверять наличие ссылки. Обычно все такие методы-проверки называются shpould_be_(название элемента
         self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
