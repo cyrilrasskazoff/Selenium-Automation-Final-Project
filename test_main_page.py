02b98b51cd6a0164a9b0c6a1e0239b8473f9cfa8
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-01-13 17:55:38 +0300

    Первый тест на основе Page Object + переход между страницами (метод .current_url)

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
-# Первый тест на основе Page Object
+# Первый тест на основе Page Object + переход между страницами (метод .current_url)
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_should_see_login_link(browser):
@@ -11,8 +12,10 @@ def test_guest_should_see_login_link(browser):
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)   # инициализируем Page Object главной страницы, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url) # инициализируем Page Object страницы логина, передаем в конструктор экземпляр драйвера и текущий URL
+    login_page.should_be_login_page() # проверка URL страницы логина
 
 # команда для запуска: pytest -v --tb=line --language=en test_main_page.py
