3b049993716003ab2fa8f65a422435d2b852aa39
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-01-13 17:29:39 +0300

    вынос селекторов во внешнюю переменную

--- pages/locators.py
+++ pages/locators.py
@@ -4,3 +4,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link") # каждый селектор — это пара: как искать и что искать
+
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
