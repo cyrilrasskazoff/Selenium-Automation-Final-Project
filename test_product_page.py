6ba2076e4e80c61d15d99ecd2145452ded4f4dc2
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-01-17 20:49:32 +0300

    тесты для страницы продукта

---
+++ test_product_page.py
@@ -0,0 +1,27 @@
+from pages.product_page import ProductPage
+
+
+def test_the_url_is_correct(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_product_page_url()
+
+
+def test_guest_should_see_add_to_cart_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_add_to_cart_btn()
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_cart()
+    product_page.solve_quiz_and_get_code()
+    product_page.correct_product()
+    product_page.correct_price()
+
+
