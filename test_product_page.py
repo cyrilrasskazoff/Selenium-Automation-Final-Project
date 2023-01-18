474033f7f3356848cd1fde8ce6bc93a2e3c40f24
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-01-18 21:06:49 +0300

    тесты для страницы продукта

--- test_product_page.py
+++ test_product_page.py
@@ -1,22 +1,29 @@
 from pages.product_page import ProductPage
+import pytest
 
 
 def test_the_url_is_correct(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.should_be_product_page_url()
 
 
 def test_guest_should_see_add_to_cart_button(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.should_be_add_to_cart_btn()
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.xfail(reason="we expect this to fail")
+@pytest.mark.parametrize('offer_id', ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+def test_guest_can_add_product_to_basket(browser, offer_id):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer_id}"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_cart()
@@ -24,4 +31,4 @@ def test_guest_can_add_product_to_basket(browser):
     product_page.correct_product()
     product_page.correct_price()
 
-
+# pytest -s -v -rx test_product_page.py
