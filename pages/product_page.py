3510ca408dd430a420936ccd81aba1731aab5eb6
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-01-22 19:50:58 +0300

    Page Object для страницы продукта

--- pages/product_page.py
+++ pages/product_page.py
@@ -27,3 +27,10 @@ class ProductPage(BasePage):
     def correct_price(self):
         assert self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text in self.browser.find_element(*ProductPageLocators.ALERT_WITH_CART_PRICE).text, "Wrong sum in cart"
 
+    def should_not_be_success_message(self): # упадет, как только увидит искомый элемент
+        assert self.is_not_element_present(*ProductPageLocators.ALERT_WITH_BOOK_TITLE), \
+            "Success message is presented, but should not be"
+
+    def should_disappear_success_message(self): # упадет, как только увидит искомый элемент
+        assert self.is_disappeared(*ProductPageLocators.ALERT_WITH_BOOK_TITLE), \
+            "Success message is presented, but should disappear"
