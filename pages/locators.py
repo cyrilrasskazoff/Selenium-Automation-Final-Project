fa175784bc470433e0d710f72b8968431bf14283
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-01-24 12:08:42 +0300

    вынос селекторов во внешнюю переменную

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link") # каждый селектор — это пара: как искать и что искать
+    VIEW_BASKET_BUTTON = (By.XPATH, "//*[@id='default']/header/div[1]/div/div[2]/span/a")
 
 
 class LoginPageLocators:
@@ -17,3 +18,8 @@ class ProductPageLocators:
     ADD_TO_CART_BTN = (By.CSS_SELECTOR, "#add_to_basket_form > button")
     ALERT_WITH_BOOK_TITLE = (By.XPATH, "//*[@id='messages']/div[1]/div/strong")
     ALERT_WITH_CART_PRICE = (By.XPATH, "//*[@id='messages']/div[3]")
+
+
+class BasketPageLocators:
+    ITEMS_IN_BASKET = (By.XPATH, "//*[@id='content_inner']/div[1]/div/h2")
+    EMPTY_BASKET_STATE = (By.XPATH, "//*[@id='content_inner']/p")
