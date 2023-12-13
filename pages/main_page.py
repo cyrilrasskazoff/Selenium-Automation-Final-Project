1ed1d4902cd17a33580335fcb965e4df8ec14c1b
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-12-13 21:07:11 +0300

    Page Object для главной страницы сайта

--- pages/main_page.py
+++ pages/main_page.py
@@ -10,9 +10,8 @@ class MainPage(BasePage):
         login_link.click()
 
     # def should_be_login_link(self): # метод, который будет проверять наличие ссылки. Обычно все такие методы-проверки называются shpould_be_(название элемента
-    #     self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+    #     self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented" # здесь ссылка неверная --> тест упадет с Exception
 
     # более сложная реализация (с пом. assert, Exceptions) метода выше
     def should_be_login_link(self):
-        # assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented" # здесь ссылка неверная --> тест упадет с Exception
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented" # * указывает на то, что мы передали именно пару, и этот кортеж нужно распаковать.
