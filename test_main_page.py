4870e1d62b7958fd541c7b8e29dbdd29c850b9a7
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-12-17 10:55:26 +0300

    Первый тест на основе Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 # Первый тест на основе Page Object + переход между страницами (метод .current_url) + негативные проверки
-from pages.main_page import MainPage
+#from pages.main_page import MainPage #- at later stages we transferred class MainPage' methods into BasePage
+from pages.base_page import BasePage
 from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
 import pytest
@@ -7,14 +8,16 @@ import pytest
 
 def test_guest_should_see_login_link_on_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    #page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object главной страницы, передаем в конструктор экземпляр
+    #page = MainPage(browser, link)
+    page = BasePage(browser, link)   # инициализируем Page Object главной страницы, передаем в конструктор экземпляр
     # драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
@@ -25,7 +28,8 @@ def test_guest_can_go_to_login_page_from_main_page(browser):
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    #page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.should_be_basket_link()
     page.go_to_basket_page()
@@ -40,7 +44,8 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
 class TestLoginFromMainPage:
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser, link)
+        #page = MainPage(browser, link)
+        page = BasePage(browser, link)
         page.open()
         page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
@@ -48,7 +53,8 @@ class TestLoginFromMainPage:
 
     def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser, link)
+        #page = MainPage(browser, link)
+        page = BasePage(browser, link)
         page.open()
         page.should_be_login_link()
 # для запуска групы login_guest: pytest -v -m login_guest test_main_page.py
